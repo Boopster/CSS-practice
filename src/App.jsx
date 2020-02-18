3aa0f5817ab31a35f972f666e1e3058e3a14771f
Rebecca Still <beckystill@hotmail.com>
2020-02-18 16:12:22 +0000

    practice fadein

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [buttonClass, setButtonClass] = useState("button-hidden");
+
+  const toggleButtonClass = () => {
+    const startTimer = setTimeout(() => setButtonClass("button fade-in"), 2000);
+  }
+
+  useEffect(() => {
+    toggleButtonClass();
+  }, []);
+
   return (
-    <div className="container">
-      <div className="parent">
-        <div className="child">
-          <h1 className="title">DinoPark&trade;</h1>
-          <button className="button">Play Game</button>
-        </div>
-      </div>
-    </div>
+    <>
+      <h1 className="title">DinoPark&trade;</h1>
+      <button className={buttonClass}>Play Game</button>
+    </>
   );
 };
 
---
+++ src/Appv1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="parent">
+        <div className="child">
+          <h1 className="title">DinoPark&trade;</h1>
+          <button className="button">Play Game</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
